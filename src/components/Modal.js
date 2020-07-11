5d22759d15f54c411a7728b32cb711b15adaf539
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2020-07-11 10:22:47 +0600

    design form field

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,7 +5,18 @@ const Modal = () => {
     return ReactDom.createPortal(
         <div className="modal">
             <div className="modal-content">
-                modal component
+                <button className="close">X</button>
+                <form>
+                    <div className="email">
+                        <label>Your Email</label>
+                        <input type="email" />
+                    </div>
+                    <div className="password">
+                        <label>Your Password</label>
+                        <input type="password" />
+                    </div>
+                    <button className="login">Login</button>
+                </form>
             </div>
         </div>
         ,
